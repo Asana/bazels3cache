192ccaf85828ae9f0b22585a5fdd1259de264a6d
Mike Morearty <mike@asana.com>
2018-01-11 12:17:21 -0800

    keep-alive
    Amazingly, The JavaScript AWS SDK seems to default to not using
    keep-alive. Turning on keep-alive speeds things up a little.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as child_process from "child_process";
 import * as http from "http";
+import * as https from "https";
 import * as AWS from "aws-sdk";
 import * as debug_ from "debug";
 import * as minimist from "minimist";
@@ -30,7 +31,15 @@ function daemonMain(args: Args, onDoneInitializing: () => void) {
     const chain = new AWS.CredentialProviderChain(null);
     chain.resolvePromise()
         .then(credentials => {
-            AWS.config.credentials = credentials;
+            AWS.config.update({
+                httpOptions: {
+                    agent: new https.Agent({
+                        keepAlive: true,
+                        keepAliveMsecs: 60000
+                    })
+                },
+                credentials: credentials
+            });
             return new AWS.S3({
                 apiVersion: "2006-03-01",
                 credentials: credentials
